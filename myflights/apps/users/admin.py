dc10c14c6b6db7a966bb74beeca1062bbe401972
Oleksandr Ignatenko <avi.cpp@gmail.com>
2019-09-26 15:02:46 +0200

    Add Form and ModelAdmin for custom User model

--- myflights/apps/users/admin.py
+++ myflights/apps/users/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .forms import UserCreationForm, UserChangeForm
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Represents User model in admin site interface
+    """
+
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+    list_display = ('email', 'is_staff', 'is_active', 'is_superuser')
+    list_filter = ('email', 'is_staff', 'is_active', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {'fields': ('is_staff', 'is_active', 'is_superuser')},
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'email',
+                    'password1',
+                    'password2',
+                    'is_staff',
+                    'is_active',
+                ),
+            },
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
---
+++ myflights/apps/users/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import forms
+from django.forms import EmailField
+
+from .models import User
+
+
+class UserCreationForm(forms.UserCreationForm):
+    """Provides functionality to create new user from admin site
+    """
+
+    class Meta:
+        model = User
+        fields = ("email",)
+        field_classes = {'email': EmailField}
+
+
+class UserChangeForm(forms.UserChangeForm):
+    """Provides functionality to change user from admin site
+    """
+
+    class Meta:
+        model = User
+        fields = ('email',)
+        field_classes = {'email': EmailField}
