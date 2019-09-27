6e4f06749d7fa17d73ff205b2e8cb94038acf3e1
Oleksandr Ignatenko <avi.cpp@gmail.com>
2019-09-27 15:05:49 +0200

    Initial Data Model
     - Add `core` app
     - Add `Airport`, `Airline`, `Route` and `Flight` models to `core` app

---
+++ myflights/apps/core/tests.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+from .models import Airline, Airport, Route, Flight
+
+
+class AirlineTests(TestCase):
+    pass
+
+
+class AirportTests(TestCase):
+    pass
+
+
+class RouteTests(TestCase):
+    pass
+
+
+class FlightTests(TestCase):
+    pass
