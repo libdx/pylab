ec946763d5218dfe46fffff5d40b254687557111
Oleksandr Ignatenko <avi.cpp@gmail.com>
2019-09-30 14:11:49 +0200

    Add tests on `core.models`

--- myflights/apps/core/models.py
+++ myflights/apps/core/models.py
@@ -23,7 +23,7 @@ class Airport(models.Model):
         _('Offset from UTC in hours'), null=True, blank=True
     )
     timezone = models.CharField(
-        _('Time zone name in tz (Olsom) format', max_length=100), null=True, blank=True
+        _('Time zone name in tz (Olsom) format'), max_length=100, null=True, blank=True
     )
     openflights_id = models.IntegerField(
         _('ID from OpenFlights database'), null=True, blank=True
@@ -39,7 +39,7 @@ class Airline(models.Model):
 
     name = models.CharField(_('Airline name'), max_length=255, null=True)
     alias = models.CharField(
-        _('Short Airline alias', max_length=100), null=True, blank=True
+        _('Short Airline alias'), max_length=100, null=True, blank=True
     )
     iata = models.CharField(
         _('3-letter IATA code'), max_length=3, null=True, blank=True
@@ -48,7 +48,7 @@ class Airline(models.Model):
         _('4-letter ICAO code'), max_length=4, null=True, blank=True
     )
     callsign = models.CharField(
-        _('Airline callsign'), max_length=50, null=True, blak=True
+        _('Airline callsign'), max_length=50, null=True, blank=True
     )
     country = CountryField(_('Country or territory where Airline is incorporated'))
     is_active = models.BooleanField(
@@ -78,6 +78,9 @@ class Route(models.Model):
     stops = models.IntegerField(_('Number of stops on route'), null=True, blank=True)
     equipment = models.CharField(_('3-letter codes for plane type'), max_length=100)
 
+    def __str__(self):
+        return f'{self.origin_airport} - {self.destination_airport}'
+
 
 class Flight(models.Model):
     """Represents single flight along it's route
@@ -86,3 +89,11 @@ class Flight(models.Model):
     route = models.ForeignKey(Route, related_name='flights', on_delete=models.CASCADE)
     departure_date = models.DateTimeField(_('Departure date time in UTC'))
     arrival_date = models.DateTimeField(_('Arrival date time in UTC'))
+
+    def __str__(self):
+        return '{} [{}] - {} [{}]'.format(
+            self.route.origin_airport,
+            self.departure_date,
+            self.route.destination_airport,
+            self.arrival_date,
+        )
