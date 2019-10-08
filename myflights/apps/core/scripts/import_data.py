4ff6e46df5c35a1b8ebba31a093d9e8d507c2176
Oleksandr Ignatenko <avi.cpp@gmail.com>
2019-10-08 11:31:39 +0200

    Add documentation on data import scripts

--- myflights/apps/core/scripts/import_data.py
+++ myflights/apps/core/scripts/import_data.py
@@ -1,5 +1,5 @@
-import json
-
+import sys
+import json 
 from django.core.exceptions import ValidationError
 from abc import ABC, abstractproperty
 from pathlib import Path
@@ -8,31 +8,45 @@ from ..models import Airline, Airport, Route
 
 
 class BaseImporter(ABC):
+    """Abstract class that provides interface and base functionality
+    to import data from JSON to database.
+    """
     Model = None
     batch_size = 100
 
-    def __init__(self, file=None):
-        self._file = file
+    def __init__(self):
+        """Initializes `BaseImporter` instance.
+        """
+
+        self.file = Path('data') / self.filename
         self.new_objects = []
 
     def _log(self, msg):
-        print(f'[{self.Model}] {msg}')
+        """Naive log to strerr using built-in print.
+
+        :param msg: message to be logged
+        :type msg: str
+        """
+
+        print(f'[{self.Model}] {msg}', file=sys.stderr)
 
     @abstractproperty
-    def filename_stem(self):
+    def filename(self):
+        """Abstract property. Provide JSON filename in data directory to be imported.
+        """
         pass
 
-    @property
-    def file(self):
-        if self._file is None:
-            return Path('data') / Path(self.filename_stem).with_suffix('.json')
-        else:
-            return self.__file
-
     def _materialize(self, item):
+        """Override to substitute identifiers in `item` with model objects
+        to satisfy relationship requirements if any.
+        """
         return item
 
     def load(self):
+        """Deserializes data from corresponding JSON file and creates model objects.
+        This also calls `_materialize` where model object's relationships could be satisfied.
+        """
+
         self._log(f'Loading data from file {self.file}')
 
         with open(self.file) as f:
@@ -51,6 +65,9 @@ class BaseImporter(ABC):
         self._log(f'Created {len(self.new_objects)} objects')
 
     def save(self):
+        """Saves all created models using `bulk_create`.
+        """
+
         self._log(f'Saving in batches=[{self.batch_size}]')
 
         self.Model.objects.bulk_create(self.new_objects, self.batch_size)
@@ -60,26 +77,38 @@ class AirlineImporter(BaseImporter):
     Model = Airline
 
     @property
-    def filename_stem(self):
-        return 'airlines'
+    def filename(self):
+        """Provides filename for JSON file containing Airlines data.
+        """
+        return 'airlines.json'
 
 
 class AirportImporter(BaseImporter):
     Model = Airport
 
     @property
-    def filename_stem(self):
-        return 'airports'
+    def filename(self):
+        """Provides filename for JSON file containing Airports data.
+        """
+        return 'airports.json'
 
 
 class RouteImporter(BaseImporter):
     Model = Route
 
     @property
-    def filename_stem(self):
-        return 'routes'
+    def filename(self):
+        """Provides filename for JSON file containing Routes data.
+        """
+        return 'routes.json'
 
     def _lookup_airport(self, openflights_id):
+        """Queries Airport object by given openflights_id.
+        Raises ValueError if object not found.
+
+        :param openflights_id: id from OpenFlights dataset.
+        :type openflights_id: int
+        """
         try:
             return Airport.objects.get(openflights_id=openflights_id)
         except Airport.DoesNotExist:
@@ -88,6 +117,12 @@ class RouteImporter(BaseImporter):
             )
 
     def _lookup_airline(self, openflights_id):
+        """Queries Airline object by given openflights_id.
+        Raises ValueError if object not found.
+
+        :param openflights_id: id from OpenFlights dataset.
+        :type openflights_id: int
+        """
         try:
             return Airline.objects.get(openflights_id=openflights_id)
         except Airline.DoesNotExist:
@@ -96,6 +131,9 @@ class RouteImporter(BaseImporter):
             )
 
     def _materialize(self, item):
+        """Substitutes airports and airline IDs with actual objects
+        to satisfy relationships in Route.
+        """
         origin_airport_id = item['origin_airport']
         destination_airport_id = item['destination_airport']
         airline_id = item['airline']
